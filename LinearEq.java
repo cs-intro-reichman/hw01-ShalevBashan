c6747413ffcdba0230f36ae89fe024d6ec061183
Shalev Bashan <shalev.bashan@post.runi.ac.il>
2023-12-21 10:29:11 +0200

    removed a redundant line of code in LinearEq and updated the pdf

--- LinearEq.java
+++ LinearEq.java
@@ -3,7 +3,6 @@ public class LinearEq {
         double a = Double.parseDouble(args[0]);
         double b = Double.parseDouble(args[1]);
         double c = Double.parseDouble(args[2]);
-        double x;
         System.out.println (a + " * x + " + b + " = " + c);
         c = c - b;
         c = c / a;
